115636ff82773c7523e658d6be3331e11eaa3b84
bingcharlesdeng <bingcharlesdeng@gmail.com>
2024-05-18 17:09:41 -0700

    Updates

--- src/portfolioData.js
+++
@@ -1,9 +0,0 @@
-export const portfolioItems = [
-  {
-    title: 'Portfolio Item 1',
-    link: 'https://yourportfolioitem.com',
-    image: 'path/to/portfolio-image1.jpg', // Optional image
-    description: 'A brief description of your portfolio item.',
-  },
-  // Add more portfolio items here
-];
