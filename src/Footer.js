d459f077f14710998bc94a1ff57a39c4d1cacba5
bingcharlesdeng <bingcharlesdeng@gmail.com>
2024-05-20 20:03:46 -0700

    added env file for esclint

--- src/Footer.js
+++ src/Footer.js
@@ -5,9 +5,9 @@ const Footer = () => {
   const year = new Date().getFullYear();
   const [socialLinksOpen, setSocialLinksOpen] = useState(false);
 
-  const toggleSocialLinks = () => {
-    setSocialLinksOpen(!socialLinksOpen);
-  };
+  // const toggleSocialLinks = () => {
+  //   setSocialLinksOpen(!socialLinksOpen);
+  // };
 
   return (
     <footer className="footer">
@@ -16,29 +16,7 @@ const Footer = () => {
           <p className="copyright">&copy; {year} Charles Deng</p>
         </div>
         <div className="social-links-container">
-          <button
-            className={`social-toggle ${socialLinksOpen ? 'open' : ''}`}
-            onClick={toggleSocialLinks}
-          >
-            <i className="fas fa-share"></i> Connect
-          </button>
-          <ul className={`social-links ${socialLinksOpen ? 'visible' : ''}`}>
-            <li>
-              <a href="#">
-                <i className="fab fa-github"></i>
-              </a>
-            </li>
-            <li>
-              <a href="#">
-                <i className="fab fa-twitter"></i>
-              </a>
-            </li>
-            <li>
-              <a href="#">
-                <i className="fab fa-linkedin"></i>
-              </a>
-            </li>
-          </ul>
+         
         </div>
       </div>
     </footer>
