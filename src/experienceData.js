8b216d24851c0fb155a9e1171a58734995e916c9
bingcharlesdeng <bingcharlesdeng@gmail.com>
2024-05-21 15:47:48 -0700

    Added definitions to Job descriptions

--- src/experienceData.js
+++ src/experienceData.js
@@ -2,14 +2,14 @@ const experienceData = [
   {
     company: 'Salesforce',
     role: 'Software Engineer',
-    dates: 'Start Date - End Date', // Replace with your actual dates
-    description: 'Describe your responsibilities and achievements at Salesforce.',
+    dates: 'June 2019 - September 2019', // Replace with your actual dates
+    description: 'Cloud Technology',
   },
   {
     company: 'Sinequa',
     role: 'Solutions Engineer',
-    dates: 'Start Date - End Date', // Replace with your actual dates
-    description: 'Describe your responsibilities and achievements at Sinequa.',
+    dates: 'January 2019 - February 2019', // Replace with your actual dates
+    description: 'Natural Language Processing Tools',
   },
 ];
 
